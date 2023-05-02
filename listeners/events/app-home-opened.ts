70572b9d73e9abe6ad633cca39b24447793a22c6
Rise Erpelding <rise@heysparkbox.com>
2023-05-01 20:35:08 -1000

    feat: add date picker to home screen

--- listeners/events/app-home-opened.ts
+++ listeners/events/app-home-opened.ts
@@ -1,8 +1,14 @@
 import { AllMiddlewareArgs, SlackEventMiddlewareArgs } from '@slack/bolt';
 
 const appHomeOpenedCallback = async ({ client, event }: AllMiddlewareArgs & SlackEventMiddlewareArgs<'app_home_opened'>) => {
+  console.log('app home opened');
   // Ignore the `app_home_opened` event for anything but the Home tab
   if (event.tab !== 'home') return;
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = (today.getMonth() + 1).toString().padStart(2, "0");
+  const day = today.getDate().toString().padStart(2, "0");
+  const todayFormatted = `${year}-${month}-${day}`;
 
   try {
     await client.views.publish({
@@ -14,14 +20,24 @@ const appHomeOpenedCallback = async ({ client, event }: AllMiddlewareArgs & Slac
             type: 'section',
             text: {
               type: 'mrkdwn',
-              text: `*Welcome home, <@${event.user}> :house:*`,
+              text: "Hello! SparkBagel is a bot that will arrange members of the channel it's added to into random groups of 3 every 2 weeks.\n\n *Please select the first date to send out new groups:*",
             },
           },
           {
             type: 'section',
             text: {
               type: 'mrkdwn',
-              text: 'Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>.',
+              text: 'Pick a date for SparkBagelBot to send out new group assignments.',
+            },
+            accessory: {
+              type: 'datepicker',
+              initial_date: todayFormatted,
+              placeholder: {
+                type: 'plain_text',
+                text: 'Select a date',
+                emoji: true,
+              },
+              action_id: 'datepicker-action',
             },
           },
         ],
